e54105f150c284b8787c8f39b2bf4600c31cdb9d
Nikita <nekpolet13@mail.ru>
2023-10-11 08:57:10 +0300

    потренировался

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import Info from "./components/info";
 import Header from "./components/header";
 import Input from "./components/input";
-import logo from "./img/logo.jpg";
-
+import logo from "./img/logo.jpg"
 
 class App extends React.Component {
     render() {
@@ -12,7 +11,7 @@ class App extends React.Component {
                 <Header title="Шапочка сайта"/>
                 <Info/>
                 <Input/>
-                <img alt="Логотип" width="400" src={logo}/>
+                <img src={logo} alt="Логотип" width="200"/>
             </div>
         )
     };
--- src/components/input.js
+++ src/components/input.js
@@ -2,23 +2,22 @@ import React from 'react';
 
 class Input extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            userData: "",
+            "inputText": "",
         }
-
     }
-
 
     render() {
         return (
             <div>
-                <input onChange={event => this.setState({userData: event.target.value})}/>
-                <h2>{this.state.userData}</h2>
+                <input onChange={event => {
+                    this.setState({"inputText": event.target.value})
+                }}/>
+                <h2>{this.state.inputText}</h2>
             </div>
         )
-    };
-
+    }
 }
 
 export default Input;
